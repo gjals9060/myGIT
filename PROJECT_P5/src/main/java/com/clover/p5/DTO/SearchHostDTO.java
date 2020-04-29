38cc297de4336b00ee8a1d9aece5a524c47398d6
gjals <gjals@DESKTOP-R3LL9VS>
2020-04-29 14:55:10 +0900

    일단 commit

---
+++ PROJECT_P5/src/main/java/com/clover/p5/DTO/SearchHostDTO.java
@@ -0,0 +1,32 @@
+package com.clover.p5.DTO;
+
+public class SearchHostDTO {
+	
+	private String sStartDate;
+	private String sEndDate;
+	private double swLatitude;
+	private double neLatitude;
+	private double swLongitude;
+	private double neLongitude;
+	private int capacity;
+	
+	public SearchHostDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SearchHostDTO(String sStartDate, String sEndDate, double swLatitude, double neLatitude, double swLongitude,
+			double neLongitude, int capacity) {
+		super();
+		this.sStartDate = sStartDate;
+		this.sEndDate = sEndDate;
+		this.swLatitude = swLatitude;
+		this.neLatitude = neLatitude;
+		this.swLongitude = swLongitude;
+		this.neLongitude = neLongitude;
+		this.capacity = capacity;
+	}
+	
+	
+	
+	
+}
--- PROJECT_P5/src/main/java/com/clover/p5/host/service/HostService.java
+++ PROJECT_P5/src/main/java/com/clover/p5/host/service/HostService.java
@@ -1,12 +1,16 @@
 package com.clover.p5.host.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.clover.p5.host.dto.*;
+import com.clover.p5.VO.HostParamVO;
+import com.clover.p5.entity.Host;
+import com.clover.p5.entity.NewHostDTO;
 
 public interface HostService {
 	
@@ -21,5 +25,25 @@ public interface HostService {
 	
 	// reservaionPurchase 이동
 	String reservationPurchase(HttpServletRequest request, Model model);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// 숙소 등록
+	boolean insertHost(NewHostDTO newHostDto);
+	
+	// 서버에 호스트 사진 등록
+	boolean storePhoto(String lastFolderName, List<MultipartFile> photos);
+	
+	// 숙소 등록시 설정한 예약불가 날짜 입력
+	int insertBlocking(int memberId, String blockingDate);
+	
+	
+	
 	
 }
