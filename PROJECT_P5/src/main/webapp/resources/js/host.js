64ba858cf9d7bc28430109ec26d85ed560aa0294
고뇽 <@DESKTOP-L3ENO2D>
2020-05-11 05:07:01 +0900

    주소 등록 만지는 중..

--- PROJECT_P5/src/main/webapp/resources/js/host.js
+++ PROJECT_P5/src/main/webapp/resources/js/host.js
@@ -1,65 +1,63 @@
-	// 알아서 저장하겠다우~
-$(window).bind("pagehide", function (event) {
-	var params = setParams(); // 저장할 정보를 담은 보따리를 받아서
-	var url = setSaveURL(); // 정보를 처리할 적절한 곳으로 보낸다.
-	$.ajax({
-		type : "POST",
-		url : url,
-		data : params
+
+
+
+	var presentURL = window.document.URL;
+	var start = presentURL.indexOf("/p5");
+	var end = presentURL.length;
+	var urlInfo = presentURL.substring(start, end);
+
+
+		// 알아서 저장하겠다우~
+	$(window).bind("pagehide", function (event) {
+		var url = setSaveURL(urlInfo); // 정보를 처리할 적절한 곳으로 보낸다.
+		var params = setParams(urlInfo); // 저장할 정보를 담은 보따리를 받아서
+		$.ajax({
+			type : "POST",
+			url : url,
+			data : params
+		});
 	});
-});
-	// 최신 정보만 보여주게써~
-$(window).bind("pageshow", function (event) {
-	var url = setRefreshURL();
-	$.ajax({
-		type : "POST",
-		url : url,
-		success : function(host){
-			refresh(host); // 화면 갱신
-		},	
-		error : function(){
-			alert("화면 갱신에 실패..");
-		}
-	}); // AJAX-END
-});
+		// 최신 정보만 보여주게써~
+	$(window).bind("pageshow", function (event) {
+	//	alert(urlInfo);
+		var url = setRefreshURL(urlInfo);
+		$.ajax({
+			type : "POST",
+			url : url,
+			success : function(host){
+				refresh(host); // 화면 갱신
+			},	
+			error : function(){
+				alert("화면 갱신에 실패..");
+			}
+		}); // AJAX-END
+	});
 
 
 	
 //////////////////////////////// setParams-END ///////////////////////////////////////////
-function setParams(){
+function setParams(urlInfo){
 	
 	
-//************************* roomType.jsp 값 세팅-END *************************************
+//========================= roomType.jsp 값 세팅-END ====================================
 	var hostTypeId = $('select[name="hostTypeId"]').val();
 	var roomTypeId = $('select[name="roomTypeId"]').val();
-//************************* facilities.jsp 값 세팅-END *************************************
+//=====================================================================================
 	
-	
-	
-//************************* roomCount.jsp 값 세팅-END *************************************
+//========================= roomCount.jsp 값 세팅-END ===================================
 	var capacity = $('input[name="capacity"]').val();
 	var roomCount = $('input[name="roomCount"]').val();
 	var bedCount = $('input[name="bedCount"]').val();
 	var bathroomCount = $('input[name="bathroomCount"]').val();
-//************************* facilities.jsp 값 세팅-END *************************************
+//=====================================================================================
 	
-	
-	
-//************************* address.jsp 값 세팅-END *************************************
-	var address;
-	var address2 = $('#address2').val().trim();
-	   if(!address2){ // 상세주소가 없으면
-		   address = $('#address1').text(); // 입력된 도로명 주소만
-	   } else{ // 있으면
-		   address = $('#address1').text() + " " + address2; // 그 뒤에 붙여서
-	   }
+//========================= address.jsp 값 세팅-END =====================================
+	var address = $('input[name="address"]').val();
 	var latitude = $('input[name="latitude"]').val();
 	var longitude = $('input[name="longitude"]').val();
-//************************* address.jsp 값 세팅-END *************************************	
+//=====================================================================================
 	
-	
-	
-//************************* facilities.jsp 값 세팅 *************************************
+//========================= facilities.jsp 값 세팅 ======================================
 	var isTv = 'N';
 	var isWifi = 'N';
 	var isAirConditioner = 'N';
@@ -72,152 +70,116 @@ function setParams(){
 	var	isElevator = 'N';
 	var	isParkingLot = 'N';
 		
-if($('input:checkbox[name="isTv"]').is(":checked") == true) {isTv = 'Y';}
-if($('input:checkbox[name="isWifi"]').is(":checked") == true) {isWifi = 'Y';}
-if($('input:checkbox[name="isAirConditioner"]').is(":checked") == true) {isAirConditioner = 'Y';}
-if($('input:checkbox[name="isAirPurifier"]').is(":checked") == true) {isAirPurifier = 'Y';}
-if($('input:checkbox[name="isHairDryer"]').is(":checked") == true) {isHairDryer = 'Y';}
-if($('input:checkbox[name="isIron"]').is(":checked") == true) {isIron = 'Y';}
-
-if($('input:checkbox[name="isKitchen"]').is(":checked") == true) {isKitchen = 'Y';}
-if($('input:checkbox[name="isWashingMachine"]').is(":checked") == true) {isWashingMachine = 'Y';}
-if($('input:checkbox[name="isElevator"]').is(":checked") == true) {isElevator = 'Y';}
-if($('input:checkbox[name="isParkingLot"]').is(":checked") == true) {isParkingLot = 'Y';}
-
-//************************* facilities.jsp 값 세팅-END *************************************
-
-
-
+	if($('input:checkbox[name="isTv"]').is(":checked") == true) {isTv = 'Y';}
+	if($('input:checkbox[name="isWifi"]').is(":checked") == true) {isWifi = 'Y';}
+	if($('input:checkbox[name="isAirConditioner"]').is(":checked") == true) {isAirConditioner = 'Y';}
+	if($('input:checkbox[name="isAirPurifier"]').is(":checked") == true) {isAirPurifier = 'Y';}
+	if($('input:checkbox[name="isHairDryer"]').is(":checked") == true) {isHairDryer = 'Y';}
+	if($('input:checkbox[name="isIron"]').is(":checked") == true) {isIron = 'Y';}
+	
+	if($('input:checkbox[name="isKitchen"]').is(":checked") == true) {isKitchen = 'Y';}
+	if($('input:checkbox[name="isWashingMachine"]').is(":checked") == true) {isWashingMachine = 'Y';}
+	if($('input:checkbox[name="isElevator"]').is(":checked") == true) {isElevator = 'Y';}
+	if($('input:checkbox[name="isParkingLot"]').is(":checked") == true) {isParkingLot = 'Y';}
+//====================================================================================================
 
 
 //******************************* params 세팅 *****************************************
 
-var paramsRoomType = { // roomType.jsp
-		hostTypeId : hostTypeId,
-		roomTypeId : roomTypeId
-};
-var paramsRoomCount = { //roomCount.jsp
-		capacity : capacity,
-		roomCount : roomCount,
-		bedCount : bedCount,
-		bathroomCount : bathroomCount
-};
-var paramsAddress = { // address.jsp
-		address : address,
-		latitude : latitude,
-		longitude : longitude
-};
-var paramsfacilities = { // facilities.jsp
-		isTv : isTv,
-		isWifi : isWifi,
-		isAirConditioner : isAirConditioner,
-		isAirPurifier : isAirPurifier,
-		isHairDryer : isHairDryer,
-		isIron : isIron,
-		
-		isKitchen : isKitchen,
-		isWashingMachine : isWashingMachine,
-		isElevator : isElevator,
-		isParkingLot : isParkingLot
-};
-
-
-	// 이탈하는 페이지(정보 저장)의 URL
-var presentURL = window.document.URL;
-
-	// AJAX로 보내줄 파라미터 보따리
-var params;
-
-
-
-
-	// 보따리에 이탈 페이지에서 저장할 정보를 담아준다 - URL로 구분
-if(presentURL == 'http://localhost:8081/p5/host/registration/roomType'){
-	params = paramsRoomType;
-}
-if(presentURL == 'http://localhost:8081/p5/host/registration/roomCount'){
-	params = paramsRoomCount;
-}
-if(presentURL == 'http://localhost:8081/p5/host/registration/address'){
-	params = paramsAddress;
-}
-if(presentURL == 'http://localhost:8081/p5/host/registration/facilities'){
-	params = paramsfacilities;
-}
-
+	var paramsRoomType = { // roomType.jsp
+			hostTypeId : hostTypeId,
+			roomTypeId : roomTypeId
+	};
+	var paramsRoomCount = { //roomCount.jsp
+			capacity : capacity,
+			roomCount : roomCount,
+			bedCount : bedCount,
+			bathroomCount : bathroomCount
+	};
+	var paramsAddress = { // address.jsp
+			address : address,
+			latitude : latitude,
+			longitude : longitude
+	};
+	var paramsfacilities = { // facilities.jsp
+			isTv : isTv,
+			isWifi : isWifi,
+			isAirConditioner : isAirConditioner,
+			isAirPurifier : isAirPurifier,
+			isHairDryer : isHairDryer,
+			isIron : isIron,
+			
+			isKitchen : isKitchen,
+			isWashingMachine : isWashingMachine,
+			isElevator : isElevator,
+			isParkingLot : isParkingLot
+	};
 //****************************** params 세팅-END *****************************************
 
-return params; // 세팅을 마치고 알맞은 정보를 담은 보따리를 return.
 
-}
-//////////////////////////////////// setParams-END //////////////////////////////////////////
+
+
+	if(urlInfo == '/p5/host/registration/roomType'){return paramsRoomType;}
+	if(urlInfo == '/p5/host/registration/roomCount'){return paramsRoomCount;}
+	if(urlInfo == '/p5/host/registration/address'){return paramsAddress;}
+	if(urlInfo == '/p5/host/registration/facilities'){return paramsfacilities;}
+
+
+
+
+}//////////////////////////////////// setParams-END //////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 //////////////////////////////////////// setSaveURL /////////////////////////////////////////////
-function setSaveURL(){
-	
-	// 이탈하는 페이지(정보 저장)의 URL
-	var presentURL = window.document.URL;
-	
-	// AJAX로 보낼 URL
-	var ajaxSaveURL;
+function setSaveURL(urlInfo){
 
-
-
-		// AJAX URL을 설정한다
-	if(presentURL == 'http://localhost:8081/p5/host/registration/roomType'){
-		ajaxSaveURL = "save/newHost"; // 등록 1단계에서 보낼 URL(newHost 세션 사용)
-	}
-	if(presentURL == 'http://localhost:8081/p5/host/registration/roomCount'){
-		ajaxSaveURL = "save/newHost"; // 등록 1단계에서 보낼 URL(newHost 세션 사용)
-	}
-	if(presentURL == 'http://localhost:8081/p5/host/registration/address'){
-		ajaxSaveURL = "save/newHost"; // 등록 1단계에서 보낼 URL(newHost 세션 사용)
-	}
-	if(presentURL == 'http://localhost:8081/p5/host/registration/facilities'){
-		ajaxSaveURL = "save/newHost"; // 등록 1단계에서 보낼 URL(newHost 세션 사용)
-	}
+//******************* 등록 1단계에서 보낼 URL(newHost 세션 사용) ****************************
+	if(urlInfo == '/p5/host/registration/roomType'){return "save/newHost";}
+	if(urlInfo == '/p5/host/registration/roomCount'){return "save/newHost";}
+	if(urlInfo == '/p5/host/registration/address'){return "save/newHost";}
+	if(urlInfo == '/p5/host/registration/facilities'){return "save/newHost";}
+//***************** 등록 1단계에서 보낼 URL(newHost 세션 사용)-END ****************************	
 	
 	
-	
-	return ajaxSaveURL; // 설정한 URL을 return.
-}
-////////////////////////////////////// setSaveURL-END /////////////////////////////////////////////
-
+}//////////////////////////////////// setSaveURL-END /////////////////////////////////////////////
 
 
 /////////////////////////////////////setRefreshURL /////////////////////////////////////////////
-function setRefreshURL(){
+function setRefreshURL(urlInfo){
 	
-	// 갱신할 페이지의 URL
-	var presentURL = window.document.URL;
+//******************* 등록 1단계에서 보낼 URL(newHost 세션 사용) ****************************
+	if(urlInfo == '/p5/host/registration/roomType'){return "refresh/newHost";}
+	if(urlInfo == '/p5/host/registration/roomCount'){return "refresh/newHost";}
+	if(urlInfo == '/p5/host/registration/address'){return "refresh/newHost";}
+	if(urlInfo == '/p5/host/registration/facilities'){return "refresh/newHost";}
+//***************** 등록 1단계에서 보낼 URL(newHost 세션 사용)-END ****************************	
 	
-	// AJAX로 보낼 URL
-	var ajaxRefreshURL;
+	
+}////////////////////////////////// setRefreshURL-END /////////////////////////////////////////////
 
 
 
-		// AJAX URL을 설정한다
-	if(presentURL == 'http://localhost:8081/p5/host/registration/roomType'){
-		ajaxRefreshURL = "refresh/newHost"; // 등록 1단계에서 보낼 URL(newHost 세션 사용)
-	}
-	if(presentURL == 'http://localhost:8081/p5/host/registration/roomCount'){
-		ajaxRefreshURL = "refresh/newHost"; // 등록 1단계에서 보낼 URL(newHost 세션 사용)
-	}
-	if(presentURL == 'http://localhost:8081/p5/host/registration/address'){
-		ajaxRefreshURL = "refresh/newHost"; // 등록 1단계에서 보낼 URL(newHost 세션 사용)
-	}
-	if(presentURL == 'http://localhost:8081/p5/host/registration/facilities'){
-		ajaxRefreshURL = "refresh/newHost"; // 등록 1단계에서 보낼 URL(newHost 세션 사용)
-	}
-	
-	
-	
-	return ajaxRefreshURL; // 설정한 URL을 return.
-}
-////////////////////////////////// setRefreshURL-END /////////////////////////////////////////////
+
+
 
 
 
@@ -258,8 +220,7 @@ function refresh(host){
 	
 	
 	
-}
-///////////////////////////////////// refresh-END //////////////////////////////////////////////
+}///////////////////////////////////// refresh-END //////////////////////////////////////////////
 
 
 
